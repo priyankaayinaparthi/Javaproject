da0de70338842ead8142f978fa99a6a81773d711
arohi <arohi@DESKTOP-P5B332M>
2022-07-06 19:14:44 +0530

    second commit

--- src/javaprojectmysample/MyfirstProgram.java
+++ src/javaprojectmysample/MyfirstProgram.java
@@ -5,6 +5,8 @@ public class MyfirstProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Priyanka");
+System.out.println("Hello Priyanka");
+System.out.println("Hello Priyanka");
 	}
 
 }

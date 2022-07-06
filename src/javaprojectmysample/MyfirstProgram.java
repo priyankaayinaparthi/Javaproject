cb4a3cf9be6a2005186046d66d97a980700c6f15
arohi <arohi@DESKTOP-P5B332M>
2022-07-06 19:11:42 +0530

    first commit..

---
+++ src/javaprojectmysample/MyfirstProgram.java
@@ -0,0 +1,10 @@
+package javaprojectmysample;
+
+public class MyfirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Priyanka");
+	}
+
+}

b94564f7dc37663800a54a2ccb772204eaa91d4a
priyankaayinaparthi <97830815+priyankaayinaparthi@users.noreply.github.com>
2022-07-12 14:57:51 +0530

    updated

--- src/javaprojectmysample/MyfirstProgram.java
+++ src/javaprojectmysample/MyfirstProgram.java
@@ -4,7 +4,7 @@ public class MyfirstProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Priyanka");
+System.out.println("Hello Priyankaaa");
 System.out.println("Hello Priyanka");
 System.out.println("Hello Priyanka");
 System.out.println("Hello Priyanka");
